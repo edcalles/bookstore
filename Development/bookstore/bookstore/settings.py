9edf3cb88357e3a1911f5c35ddac6570d7a97e7a
Edward Calles <ed.calles00@gmail.com>
2023-06-21 00:26:03 -0500

    added email settings to settings.py

--- Development/bookstore/bookstore/settings.py
+++ Development/bookstore/bookstore/settings.py
@@ -126,5 +126,15 @@ STATIC_URL = '/static/'
 # registration
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/store'
 
 SITE_ID = 1
+
+#email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "ed.calles00@gmail.com"
+EMAIL_HOST_PASSWORD = "rqphhisdkwbonrks"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "books@mysterybooks.com"
