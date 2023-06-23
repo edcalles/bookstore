77e1effff491413b5b2b578313d305893ab1f50e
Edward Calles <ed.calles00@gmail.com>
2023-06-23 03:18:34 -0500

    created cart

--- Development/bookstore/store/urls.py
+++ Development/bookstore/store/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('book/<int:book_id>', views.book_details, name='book_details')
+    path('book/<int:book_id>', views.book_details, name='book_details'),
+    path('add/<int:book_id>', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:book_id>', views.remove_from_cart, name='remove_from_cart'),
+    path('cart', views.cart, name='cart'),
 ]
